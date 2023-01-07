2d0f6dd659480b28b6ce41b51aa4756e8b4d6242
Björn Monke <61793599+Piovoso@users.noreply.github.com>
2023-01-07 21:57:23 +0200

    Goat 1.1.0

---
+++ script.js
@@ -0,0 +1,13 @@
+function show(){
+    document.getElementById('About-Section').style.height='400px'
+    document.getElementById('About-Section').style.display="block"
+    document.getElementById('About-Section').style.display="none"
+}
+function hidden(){
+    document.getElementById('About-Section').style.height='0px'
+    document.getElementById('About-Section').style.display="inline"
+    document.getElementById('About-Section').style.display="none"
+}
+
+function Lighing_on(){
+}
